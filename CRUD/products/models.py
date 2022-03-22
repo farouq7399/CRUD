2e476cc80f0180aa18dd04e325cb92905c921584
OmarElmahdy7399 <73123128+OmarElmahdy7399@users.noreply.github.com>
2022-03-22 07:42:39 +0200

    change in settings added base dir added templates and set up skeleton temp

---
+++ CRUD/products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class product(models.Model):
+    product_name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.product_name
---
+++ CRUD/products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect, reverse
+from .models import product
+from .forms import ProductForm
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+
+
+class ProductList(LoginRequiredMixin, ListView):
+    template_name = 'home_page.html'
+    queryset = product.objects.all()
+    context_object_name = 'products'
