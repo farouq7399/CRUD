b8f30fefbb691ed958689d65161f2efa13a38ef0
OmarElmahdy7399 <73123128+OmarElmahdy7399@users.noreply.github.com>
2022-04-01 10:14:12 +0200

    added the serializers and the delete still working on fixing bugs of not reflecting on the back end database

--- CRUD/products/urls.py
+++ CRUD/products/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ProductList, UsersList, CreateProduct, detailsProduct
+from .views import ProductList, CreateProduct, detailsProduct, DeleteProduct
 
 urlpatterns = [
     path('list/', ProductList.as_view(), name="list"),
-    path('list_user/', UsersList.as_view()),
     path('create_product/', CreateProduct.as_view(), name="create_product"),
     path('details_product/<int:pk>', detailsProduct.as_view(), name="details_product"),
+    path('details_product/<int:pk>', detailsProduct.as_view(), name="details_product"),
+    path('delete_product/<int:pk>', DeleteProduct.as_view(), name="delete_product"),
 ]
